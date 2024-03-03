4dd9a25383e004d60a9bfd5c4e45870be4ba8bd6
maxdmb9 <advance4@rambler.ru>
2024-03-03 18:27:40 +0700

    2

---
+++ I/2/name.py
@@ -0,0 +1,28 @@
+name = "ada lovelace" 
+print(name.title()) 
+print(name.upper()) 
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = ("Hi, " + full_name.title() + "!")
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+print(favorite_language) 
