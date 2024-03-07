2dff9f2e032ffa38d18a16f6914fb193622c97ff
maxdmb9 <advance4@rambler.ru>
2024-03-07 21:43:28 +0700

    1

---
+++ I/3/bicycles.py
@@ -0,0 +1,7 @@
+#Работа со списками
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[2].title())
+print(bicycles[-1].title())
+message = "My first bicycle was a " + bicycles[2].title() + "."
+print(message)
