cc294df81c4f0803a38ba223fa26d334865327b9
maxdmb9 <advance4@rambler.ru>
2024-03-25 18:01:37 +0700

    4-1, 4-2 exercise

---
+++ I/4/chapter_4-1.py
@@ -0,0 +1,5 @@
+pizzas = ['овощи', 'сыр', 'морепродукты']
+for pizza in pizzas:
+    print(pizza)
+    print("Я очень люблю " + pizza +" пиццу")
+print("I really love pizza!")
---
+++ I/4/chapter_4-2.py
@@ -0,0 +1,4 @@
+animals = ['кошки', 'собаки', 'драконы']
+for animal in animals:
+    print(animal.title() + " хорошие домашние животные")
+print("Одни из лучших животных")
