36cf68894a2864a1e46a931190acd33ae4afb1c5
maxdmb9 <advance4@rambler.ru>
2024-03-26 21:01:35 +0700

    4

--- I/4/squares.py
+++ I/4/squares.py
@@ -1,6 +1,12 @@
 #Возведение в степень
-squares = [] #просто пустой список
-for value in range(1,11): #в value поместим значения от 1 до 10
-    square = value**2 #значения в value возведем во 2 степень и сохраним в square
-    squares.append(square) #присоединение к списку squares
-print(squares) #вывод
+squares = [] #1)просто пустой список
+for value in range(1,11): #2)в value поместим значения от 1 до 10
+    square = value**2 #3)значения в value возведем во 2 степень и сохраним в square
+    squares.append(square) #4)присоединение к списку squares
+print(squares) #5)вывод
+
+squares = [] #1)просто пустой список
+for value in range(1,22): #2)в value поместим значения от 1 до 10
+    #square = value**2 #3)значения в value возведем во 2 степень и сохраним в square
+    squares.append(value**3) #4)присоединение к списку squares
+print(squares) #5)вывод
