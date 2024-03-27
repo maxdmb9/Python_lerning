f5e53f17e569ec05fdbf2efdc46fb7f93f57dcde
maxdmb9 <advance4@rambler.ru>
2024-03-27 22:14:11 +0700

    numbers

---
+++ I/4/chapter_4-7.py
@@ -0,0 +1,2 @@
+for odd_numbers in range(3,30,3):
+    print(odd_numbers)
--- I/4/squares.py
+++ I/4/squares.py
@@ -9,4 +9,8 @@ squares = [] #1)просто пустой список
 for value in range(1,22): #2)в value поместим значения от 1 до 10
     #square = value**2 #3)значения в value возведем во 2 степень и сохраним в square
     squares.append(value**3) #4)присоединение к списку squares
-print(squares) #5)вывод
+print('\n', squares) #5)вывод
+
+#Генератор списка
+squares = [value**2 for value in range(1,22)]
+print('\n', squares)
