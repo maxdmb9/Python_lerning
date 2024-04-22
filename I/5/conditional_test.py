2498518f489a80924ed3006d9df8bc3a19465654
maxdmb9 <advance4@rambler.ru>
2024-04-22 21:05:00 +0700

    23

--- I/5/conditional_test.py
+++ I/5/conditional_test.py
@@ -8,4 +8,31 @@ for one_cat in cats:
     if one_cat == 'SERAYA':
         print(one_cat.lower())
     else: 
-        print(one_cat.title())   
+        print(one_cat.title())
+
+#Сравнение чисел
+a =11
+b =26
+c =33
+if a > b:
+    print('true')    
+else:
+    print('false')    
+
+if a<b:
+    print('верно')   
+
+answer = 3
+if answer != 15:
+    print('this true ')
+
+d=b+c
+
+if d >= c:
+    print(d)
+
+if c <= a:
+    print(c)   
+
+if d>a or c<=d:
+    print('yes')    
