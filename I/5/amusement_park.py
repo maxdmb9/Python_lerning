0dd7a38b41ff0c7f262295a48fa0c548c3f8b26f
maxdmb9 <advance4@rambler.ru>
2024-05-01 22:12:10 +0700

    if elif else

---
+++ I/5/amusement_park.py
@@ -0,0 +1,18 @@
+#Пример использование if elif else
+# age = 12
+# if age <4:
+#     print("Your admission cost is $0.")
+# elif age <18:
+#     print("Your admission cost is $5.")
+# else:
+#     print("Your admission cost is $10.")   
+
+#упростим пример выше 
+age = 12
+if age < 4:
+    price = 0
+elif age <18:
+    price = 5
+else:
+    price = 10
+print("Your admission cost is $" + str(price) + ".")
