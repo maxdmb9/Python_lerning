770a9c7db5128e1622f269941c0c3d75d8a49d42
maxdmb9 <advance4@rambler.ru>
2024-05-16 21:30:39 +0700

    if

---
+++ I/5/chapter_5-7.py
@@ -0,0 +1,14 @@
+favorite_fruits = ['банан','апельсин','яблоко','киви']
+
+if 'манго' in favorite_fruits:
+    print('Любишь манго')
+if 'киви' in favorite_fruits:
+    print('Любишь киви')    
+if 'ананас' in favorite_fruits:
+    print('Любишь ананас')
+if 'банан' in favorite_fruits:
+    print('Любишь банан')
+if 'авокадо' in favorite_fruits:
+    print('Любишь авокадо')
+
+print('\nВаши любимые фрукты')     
--- I/5/toppings.py
+++ I/5/toppings.py
@@ -11,12 +11,22 @@
 # print("\nFinished making your pizza!")
 
 #Используем if elif else
-reguest_toppings = ['muchrooms', 'extra cheese']
-if 'mushrooms' in reguest_toppings:
-    print("Adding mushrooms.")
-elif 'peppironi' in reguest_toppings:
-    print("Adding peppironi.")
-elif 'extra cheese' in reguest_toppings:
-    print("Adding extra cheese.")
+# reguest_toppings = ['muchrooms', 'extra cheese']
+# if 'mushrooms' in reguest_toppings:
+#     print("Adding mushrooms.")
+# elif 'peppironi' in reguest_toppings:
+#     print("Adding peppironi.")
+# elif 'extra cheese' in reguest_toppings:
+#     print("Adding extra cheese.")
+
+# print("\nFinished making your pizza!")
+
+#используем цикл for
+requested_toppings = ['muchrooms', 'extra cheese', 'green peppers']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!")
