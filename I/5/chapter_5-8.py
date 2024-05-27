7347a3007c5b522850097b16178d54327b8fb7bc
maxdmb9 <advance4@rambler.ru>
2024-05-27 20:53:53 +0700

    1

---
+++ I/5/chapter_5-8.py
@@ -0,0 +1,7 @@
+# Hello Admin:
+users = ['Leo','Anna','Deni','admin','lin']
+for user in users:
+    if user in 'admin':
+        print("Welcome")
+    else:
+        print("Hi " + user+ " you are welcome.")
