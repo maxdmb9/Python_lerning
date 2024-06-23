8a65ffe10a204152838bf0b4e987fce3c934e5ec
maxdmb9 <advance4@rambler.ru>
2024-06-23 23:10:17 +0700

    ew

--- I/5/chapter_5-10.py
+++ I/5/chapter_5-10.py
@@ -1,4 +1,11 @@
 #Проверка имен пользователей на уникальность.
-current_users = ['','','','','','','','']
+current_users = ['Yana','vika','Maria','lena','katya','nina','galya','lida']
 
-new_users = ['','','','','','','','']
+new_users = ['vera','yana','olya','maria','kristina','dasha','mitra','leya']
+
+
+for current_user in current_users:
+    if current_user.lower() not in new_users:
+        print("You can use this name " + current_user + ".")
+    else:
+        print("This name " + current_user + " is busy")    
