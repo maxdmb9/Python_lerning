39699842d44cccde90954667ce9687a3dee7c643
maxdmb9 <advance4@rambler.ru>
2024-06-29 22:57:55 +0700

    6 chapter

--- I/5/chapter_5-11.py
+++ I/5/chapter_5-11.py
@@ -2,6 +2,13 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for number in numbers:
-    print(number)
+    if number == 1:
+        print(number,"st")
+    elif number == 2:
+        print(number, "nd")    
+    elif number == 3:
+        print(number, "rd")
+    else:
+        print(number, "th")    
 
     
