37ad5859e709ab1be644b9e843e6201ed947ea81
maxdmb9 <advance4@rambler.ru>
2024-07-04 23:15:22 +0700

    rt

--- I/6/favorite_languages.py
+++ I/6/favorite_languages.py
@@ -1,10 +1,13 @@
 # Словарь с однотипными объектами
 favorite_languages = {
     'jen': 'python',
-    'sarah': 'c',
+    'sarah': 'c++',
     'edward': 'ruby',
     'phil': 'python',
 }
 print("Sarah`s favorite language is " + 
       favorite_languages['sarah'].title() +
       ".")
+for name, language in favorite_languages.items():
+    print("Name: " + name.title() + "`s favorite language is " 
+          + language.title() + ".")
