9b7c25e95f7f6f1bf431e2e7e166e5f0095b2dbb
maxdmb9 <advance4@rambler.ru>
2024-07-12 22:04:02 +0700

    mess

--- I/6/aliens.py
+++ I/6/aliens.py
@@ -76,8 +76,18 @@ for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
 # Вывод первых 5 пришельцев:
-for alien in aliens[:5]:
+for alien in aliens[:7]:
     print(alien)
 print("...")
 
