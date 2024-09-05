acae9f3727573c8260aed2707593c1803d2bb97d
maxdmb9 <advance4@rambler.ru>
2024-09-05 22:16:59 +0700

    104

--- I/6/aliens.py
+++ I/6/aliens.py
@@ -7,6 +7,7 @@
 
 #Обращение к значниям в словаре
 # alien_0 = {'color': 'green', 'points':5}
+#print(alien_0['color'])
 
 # new_points = alien_0['points']  
 # print("You just earned " + str(new_points) + " points!")
